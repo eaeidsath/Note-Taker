13813d40f112bdf0b9e9d17e062d8dd2a1975796
EE <evelyneidsath@gmail.com>
2024-01-30 13:56:10 -0800

    set up server.js requirements and routes, create folder for routes and middleware

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js')
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+//get route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//get route for notes page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
