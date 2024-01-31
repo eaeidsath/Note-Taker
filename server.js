5c798abce70b5321678cb732a5d93dc590267875
EE <evelyneidsath@gmail.com>
2024-01-30 16:06:45 -0800

    create clog.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-/* const { clog } = require('./middleware/clog'); */
+const { clog } = require('./middleware/clog.js');
 const path = require('path');
 const api = require('./routes/index.js')
 const PORT = 3001;
 
 const app = express();
 
-/* app.use(clog); */
+app.use(clog);
 
+//middleware for parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Send all the requests that begin with /api to the index.js in the routes folder
 app.use('/api', api);
 
+// static pages
 app.use(express.static('public'));
 
 //get route for homepage
