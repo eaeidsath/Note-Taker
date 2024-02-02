283473f8041305f53470ec6b25db313dfa45f11e
eaeidsath <evelyneidsath@gmail.com>
2024-02-02 02:37:16 -0800

    create delete route for notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const fs = require('fs');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 // get route to retrieve notes from db.json
 notes.get('/', (req, res) =>
@@ -19,7 +20,7 @@ notes.post('/', (req, res) => {
         title,
         text,
         // this function gives a unique id to each note
-        note_id: uuidv4(),
+        id: uuidv4(),
       };
   
       readAndAppend(newNote, './db/db.json');
@@ -35,9 +36,20 @@ notes.post('/', (req, res) => {
     }
 });
 
-notes.delete("/:id", (req, res) => {
-    if (req.params.id) {
-
+notes.delete("/:id", async (req, res) => {
+    try {
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        const db = JSON.parse(data);
+        const index = db.findIndex((notes) =>
+        notes.id === req.params.id
+        );
+        db.splice(index, 1);
+        writeToFile('./db/db.json', db);
+        })
+        location.reload();
+        res.status(200).json({status: 'success'});
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
--- server.js
+++ server.js
@@ -8,22 +8,22 @@ const app = express();
 
 app.use(clog);
 
-//middleware for parsing JSON
+// middleware for parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Send all the requests that begin with /api to the index.js in the routes folder
 app.use('/api', api);
 
-// static pages
+// sets static pages
 app.use(express.static('public'));
 
-//get route for homepage
+// get route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-//get route for notes page
+// get route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
